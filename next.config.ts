8754947ba42563f1425a6f06e3ecf9df6a5df637
satishCyrus <satishchaubey02@gmail.com>
2026-09-02 00:05:51 +0530

    fix: remove output standalone for Vercel deployment compatibility

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   reactStrictMode: false,
   images: {
     unoptimized: true, // disable image optimization if using CDN
